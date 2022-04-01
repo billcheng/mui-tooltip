4ea11b2b1836c0be46e1a822ca4976f5b3fa83d4
Bill Cheng <cccheng118@yahoo.com>
2022-04-01 12:44:13 -0500

    Use ClickAwayListener

--- src/components/ImageLink.js
+++ src/components/ImageLink.js
@@ -1,56 +1,34 @@
-import { useState, useRef, useEffect } from "react";
-import { Tooltip, Button } from "@mui/material";
+import { useState } from "react";
+import { Tooltip, Button, ClickAwayListener } from "@mui/material";
 
-function useClickOutside(ref1, ref2, callback) {
-  const handleClick = (e) => {
-    console.log({
-      ref1: ref1?.current.contains(e.target),
-      ref2: ref2?.current.contains(e.target),
-    });
-    if (
-      !ref1?.current.contains(e.target) &&
-      !ref2?.current.contains(e.target)
-    ) {
-      callback();
-    }
-  };
-  useEffect(() => {
-    document.addEventListener("click", handleClick);
-    return () => {
-      document.removeEventListener("click", handleClick);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [ref1, ref2, callback]);
-}
-
-export default function ImageLink({imageLink, children}) {
-  const [openTooltip, setOpenTooltip] = useState(false);
-
-  const ref1 = useRef();
-  const ref2 = useRef();
-  useClickOutside(ref1, ref2, () => {
-    if (openTooltip) {
-      setOpenTooltip(false);
-    }
-  });
+export default function ImageLink({ imageLink, children }) {
+  const [tooltipOpened, setTooltipOpened] = useState(false);
 
   const handleOpenTooltip = () => {
-    setOpenTooltip(true);
+    setTooltipOpened(true);
+  };
+
+  const handleCloseTooltip = () => {
+    setTooltipOpened(false);
   };
 
   return (
-    <Tooltip
-      title={
-        <div ref={ref1}>
-          <img src={imageLink} alt="" />
-        </div>
-      }
-      disableHoverListener
-      open={openTooltip}
-    >
-      <Button ref={ref2} onClick={handleOpenTooltip}>
-        {children}
-      </Button>
-    </Tooltip>
+    <ClickAwayListener onClickAway={handleCloseTooltip}>
+      <div>
+        <Tooltip
+          PopperProps={{
+            disablePortal: true,
+          }}
+          onClose={handleCloseTooltip}
+          open={tooltipOpened}
+          disableFocusListener
+          disableHoverListener
+          disableTouchListener
+          title={<img src={imageLink} alt="" />}
+        >
+          <Button onClick={handleOpenTooltip}>{children}</Button>
+        </Tooltip>
+      </div>
+    </ClickAwayListener>
   );
 }
